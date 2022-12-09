20e1fb6115de03eb6f3b2cd9868caf7f29b95f4a
Timothee Techer <timotheetecher.dev@gmail.com>
2022-12-09 11:11:15 +0100

    wazaaaaaaa

--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ const fnAdd2 = addFactory(2);
 const fnAdd5 = addFactory(5);
 const fnAdd10 = addFactory(10);
 
-console.log(fnAdd2(3));
-console.log(fnAdd5(3));
-console.log(fnAdd10(3));
+console.log("wazaaa");
+console.log("wazaaaaaaaaaaa");
+console.log("wazaaaaaaaaaaaaaaaaaaaaaaaaaa");
